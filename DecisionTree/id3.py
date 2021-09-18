9090e69c1fc3351dd5e5e4822cba3436a8019b9b
Vijay Bajracharya <bajracharyavijay@gmail.com>
2021-09-18 16:12:52 -0600

    Calculating info gain. Need to double check values

--- DecisionTree/id3.py
+++ DecisionTree/id3.py
@@ -10,18 +10,53 @@ class Node:
 class DecisionTree:
 
     def cross_entropy(self, attribute, atr_name):
-        freq = attribute.groupby([atr_name,"label"]).size().reset_index(name="Count")
-        pass
+        num_train = attribute.shape[0]
+
+        freq_by_atr = attribute.groupby([atr_name]).size().reset_index(name="Count")
+        freq_by_row = attribute.groupby([atr_name,"label"]).size().reset_index(name="Count")
+
+        total_entropy = 0.0
+        feature_counts = {}
+
+        for index, row in freq_by_atr.iterrows():
+            feature_counts[row[atr_name]] = row["Count"] 
+
+        for index, row in freq_by_row.iterrows():
+            prob = row["Count"]/feature_counts[row[atr_name]]
+            entropy = -prob*np.log2(prob)
+            total_entropy += entropy * (feature_counts[row[atr_name]]/num_train)
+
+        return total_entropy
 
     def information_gain(self, subset, atr_name):
+        # Calculate entropy of the whole subset
+        values, counts = np.unique(subset["label"], return_counts=True)
+        sum_counts = np.sum(counts)
+        total_entropy = 0.0
+        for i in range(len(counts)):
+            prob = counts[i]/sum_counts
+            total_entropy += -prob*np.log2(prob)
+
+        # Calculate entropy of particular attribute
         index = subset.columns.get_loc(atr_name)
-        self.cross_entropy(subset.iloc[:, np.r_[index,6]], atr_name)
-        pass
+        atr_entropy = self.cross_entropy(subset.iloc[:, np.r_[index,6]], atr_name)
+
+        # Return the information gain
+        return total_entropy - atr_entropy
+
 
     def id3(self, subset, train, attributes, labels):
-        info_gain = []
+        # Base case check for same labels
+        values, counts = np.unique(subset["label"], return_counts=True)
+        if len(counts) <= 1:
+            return values[0]
+        
+        # Find attribute that gives maximum information gain
+        info_gain = {}
         for atr_name in attributes:
-            info_gain.append(self.information_gain(subset, atr_name))
+            info_gain[atr_name] = self.information_gain(subset, atr_name)
+
+        print(info_gain)
 
 if __name__ == "__main__":
     cols = '''buying,
