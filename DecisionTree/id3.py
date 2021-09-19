25c00c650090a8daf45ce330bd6f37324f24abc6
Vijay Bajracharya <bajracharyavijay@gmail.com>
2021-09-18 18:07:17 -0600

    Created decision tree without depth feature

--- DecisionTree/id3.py
+++ DecisionTree/id3.py
@@ -3,24 +3,28 @@ import pandas as pd
 import numpy as np
 
 class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
+    def __init__(self):
+        self.value = None
+        self.edge = None
 
-class DecisionTree:
+class DecisionTreeClassifier:
 
     def cross_entropy(self, attribute, atr_name):
+        # Get the number of rows
         num_train = attribute.shape[0]
 
+        # Grouping to make it easier to calculate probabilities
         freq_by_atr = attribute.groupby([atr_name]).size().reset_index(name="Count")
         freq_by_row = attribute.groupby([atr_name,"label"]).size().reset_index(name="Count")
 
         total_entropy = 0.0
         feature_counts = {}
 
+        # Count the total for each value
         for index, row in freq_by_atr.iterrows():
             feature_counts[row[atr_name]] = row["Count"] 
 
+        # Calculate entropy of attribute based on value
         for index, row in freq_by_row.iterrows():
             prob = row["Count"]/feature_counts[row[atr_name]]
             entropy = -prob*np.log2(prob)
@@ -45,18 +49,47 @@ class DecisionTree:
         return total_entropy - atr_entropy
 
 
-    def id3(self, subset, train, attributes, labels):
+    def create_tree(self, data, train, attributes, labels, node=None, depth=None):
         # Base case check for same labels
-        values, counts = np.unique(subset["label"], return_counts=True)
+        values, counts = np.unique(data["label"], return_counts=True)
         if len(counts) <= 1:
-            return values[0]
-        
-        # Find attribute that gives maximum information gain
-        info_gain = {}
-        for atr_name in attributes:
-            info_gain[atr_name] = self.information_gain(subset, atr_name)
+            # Return the label as a 
+            if not node:
+                node = Node()
+            node.value = values[0]
+            return node
+        # Base case check for empty attributes
+        elif len(attributes) < 1:
+            # Return most common label
+            if not node:
+                node = Node()
+            node.value = values[np.argmax(counts)]
+            return node
+        else:
+            if not node:
+                node = Node()
 
-        print(info_gain)
+            # Find attribute that gives maximum information gain
+            info_gain = {}
+            for atr_name in attributes:
+                info_gain[atr_name] = self.information_gain(data, atr_name)
+            best_attribute = max(info_gain, key=info_gain.get)
+
+            # Create tree with best_attribute at root
+            node.value = best_attribute
+            node.edge = []
+
+            # Remove best_attribute from attributes list
+            attributes.remove(best_attribute)
+
+            for branch in np.unique(data[best_attribute]):
+                child = Node()
+                node.edge.append(child)
+                subset = data.loc[data[best_attribute] == branch]
+                child = self.create_tree(subset, train, attributes, labels, child, depth)
+
+            return node
+
 
 if __name__ == "__main__":
     cols = '''buying,
@@ -76,7 +109,7 @@ if __name__ == "__main__":
     train = pd.read_csv("utah-cs5350-fa21/DecisionTree/train.csv", names=table)
     test = pd.read_csv("utah-cs5350-fa21/DecisionTree/test.csv", names=table)
 
-    DecisionTree().id3(train, train, attributes, labels)
+    tree = DecisionTreeClassifier().create_tree(train, train, attributes, labels)
 
 
 
