363c5d9e4283e39a18c1a67663ee1f4d24a370df
Vijay Bajracharya <bajracharyavijay@gmail.com>
2021-10-11 18:23:52 -0600

    batch descent v1.1

--- LinearRegression/grad_desc.py
+++ LinearRegression/grad_desc.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-cost_list = []
-weight_list = []
-grad_list = []
+COST_LIST = []
+WEIGHT_LIST = []
+GRAD_LIST = []
+STEP_COUNT = 0
+LEARNING_RATE = 0.25
 
 def read_csv_basic():
     # Loading the Concrete dataset
@@ -27,7 +29,12 @@ def read_csv_basic():
     train = train.to_numpy()
     y = y.to_numpy()[:,np.newaxis]
 
-    return train, test, y
+    y_test = test["label"].copy()
+    test = test.iloc[:, 0:-1]
+    test = test.to_numpy()
+    y_test = y_test.to_numpy()[:,np.newaxis]
+
+    return train, test, y, y_test
 
 def read_csv():
     # Loading the Concrete dataset
@@ -76,55 +83,48 @@ def LMScost_gradient(train, weights, y):
     grad = -1 * np.dot(error.T, train) 
     return grad / m
 
-# def LMScost(train, weights, y):
-#     sum = 0.0
-#     num_train = train.shape[0]
-#     for i in range(num_train):
-#         score = np.dot(train[i], weights.T)
-#         error = y[i] - score
-#         sqr_error = np.square(error)
-#         sum = sum + sqr_error[0]
-    
-#     return sum/2
-
-# def LMScost_gradient(train, weights, y):
-#     grad = np.zeros(train.shape[1])[np.newaxis, :]
-#     num_train = train.shape[0]
-#     for i in range(num_train):
-#         score = np.dot(train[i], weights.T)
-#         error = y[i] - score
-#         grad = grad + train[i] * error
-
-#     grad = -1 * grad
-#     return grad / train.shape[1]
-
-def batch_descent(train, y, learning_rate=0.25, threshold=10e-6):
+def batch_descent(train, y, learning_rate=LEARNING_RATE, threshold=10e-6):
     # Initialize weight vector to 0
+    global STEP_COUNT
     weights = np.zeros((1, train.shape[1]))
     while True:
-        weight_list.append(weights)
+        WEIGHT_LIST.append(weights)
 
         # Calculate LMS cost
         cost = LMScost(train, weights, y)
-        cost_list.append(cost)
+        COST_LIST.append(cost)
 
         # Calculate gradient
         grad = LMScost_gradient(train, weights, y)
-        grad_list.append(grad)
+        GRAD_LIST.append(grad)
 
         # Update weights
         weights = weights - learning_rate * grad
 
+        # Count steps for plotting
+        STEP_COUNT  = STEP_COUNT + 1
+
         # Error difference magnitude
-        diff = np.linalg.norm(weights - weight_list[-1])
+        diff = np.linalg.norm(weights - WEIGHT_LIST[-1])
         if diff < threshold:
-            weight_list.append(weights)
+            WEIGHT_LIST.append(weights)
             break
 
-def predict(test, y, weights):
+def SGD():
     pass
 
+def predict(test, y, weights):
+    cost = LMScost(test, weights, y)
+    print(f"Function value (Test): {cost}\r\n")
+
 if __name__ == "__main__":
-    train, test, y, y_test = read_csv()
+    train, test, y, y_test = read_csv_basic()
     batch_descent(train, y)
-    predict(test, y_test, weight_list[-1])
+
+    plt.step(x=np.arange(0, STEP_COUNT, 1),y=COST_LIST)
+    plt.show()
+    print(f"Wight Vector: {WEIGHT_LIST[-1]}\r\n")
+    print(f"Learning Rate: {LEARNING_RATE}\r\n")
+
+    predict(test, y_test, WEIGHT_LIST[-1])
+
