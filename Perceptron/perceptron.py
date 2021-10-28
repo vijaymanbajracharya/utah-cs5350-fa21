f95b7e56d669aa2b6a2b08e5fc957fc231baa246
Vijay Bajracharya <bajracharyavijay@gmail.com>
2021-10-27 19:53:37 -0600

    Implemented average perceptron

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -59,6 +59,15 @@ def calculate_error_voted(X, y, weights):
 
     return errors / X.shape[0]
 
+def calculate_error_average(X, y, average):
+    predictions = np.sign(np.dot(X, average))
+    errors = 0
+    for i in range(X.shape[0]):
+        if predictions[i] != y[i]:
+            errors += 1
+
+    return errors / X.shape[0]
+
 def shuffle(X, y):
     shuffler = np.random.permutation(len(X))
     X_shuffled = X[shuffler]
@@ -95,16 +104,34 @@ class VotedPerceptron:
 
         for i in range(EPOCHS):
             index = 0
-            train_shuffled, y_shuffled = shuffle(train, y)
-            for row in train_shuffled:
+            for row in train:
                 row = row[:,np.newaxis]
                 prediction = np.sign(np.dot(weights.T, row))
-                if y_shuffled[index] != prediction:
-                    weights = weights + LEARNING_RATE * (y_shuffled[index] * row)
+                if y[index] != prediction:
+                    weights = weights + LEARNING_RATE * (y[index] * row)
                     self.weights_list.append((weights, 1))
                 else:
                     self.weights_list[-1] = (weights, self.weights_list[-1][1] + 1)
 
+                index += 1
+
+class AveragePerceptron:
+    def __init__(self):
+        self.average = None
+
+    def fit(self, train, y):
+        weights = np.zeros((train.shape[1], 1))
+        self.average = np.zeros((train.shape[1], 1))
+
+        for i in range(EPOCHS):
+            index = 0
+            for row in train:
+                row = row[:,np.newaxis]
+                prediction = np.sign(np.dot(weights.T, row))
+                if y[index] != prediction:
+                    weights = weights + LEARNING_RATE * (y[index] * row)
+
+                self.average += weights
                 index += 1
 
 if __name__ == "__main__":
@@ -127,6 +154,14 @@ if __name__ == "__main__":
     test_error = calculate_error_voted(test, y_test, vp.weights_list)
     print(f"Test Error: {test_error*100}%")
     print("\r\n")
+
+    # Average Perceptron
+    print("Average Perceptron Algorithm")
+    train, test, y, y_test = read_csv()
+    ap = AveragePerceptron()
+    ap.fit(train, y)
+    test_error = calculate_error_average(test, y_test, ap.average)
+    print(f"Test Error: {test_error*100}%")
 
 
 
