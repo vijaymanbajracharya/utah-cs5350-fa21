27faf55d0b96a27a8beadd3cc20db27ebfd06bf8
Vijay Bajracharya <bajracharyavijay@gmail.com>
2021-11-18 09:38:01 -0700

    Working gaussian kernel dual solution

--- SVM/svm.py
+++ SVM/svm.py
@@ -49,6 +49,10 @@ def shuffle(X, y):
     y_shuffled = y[shuffler]
     return X_shuffled, y_shuffled
 
+def gaussian_kernel(xi, xj, gamma=0.1):
+    kernel = np.exp((-np.linalg.norm(xi-xj)**2) / gamma)
+    return kernel
+
 class svm:
     def __init__(self, C=100/873, lr_schedule=0):
         self.C = C
@@ -94,22 +98,33 @@ class svm:
         return errors / X.shape[0]
 
 class svm_dual:
-    def __init__(self, C=100/873):
+    def __init__(self, C=100/873, use_kernel=0, gamma=0.1):
         self.C = C
         self.alphas = None
-        self.H = None
+        self.G = None
         self.learning_rate = LR_0
         self.weights = None
         self.bias = None
+        self.use_kernel = use_kernel
+        self.gamma = gamma
 
     def obj_func(self, alphas):
-        return 0.5 * np.dot(alphas.T, np.dot(self.H, alphas)) - np.sum(alphas)
+        return 0.5 * np.dot(alphas.T, np.dot(self.G, alphas)) - np.sum(alphas)
 
     def fit(self, X, y):
-        self.H = np.dot(X, X.T)*np.dot(y, y.T)
+        if self.use_kernel == 0:
+            self.G = np.dot(X, X.T)*np.dot(y, y.T)
+        else:
+            self.G = np.zeros((X.shape[0],X.shape[0]))
+            for i in range(X.shape[0]):
+                for j in range(X.shape[0]):
+                    self.G[i,j] = gaussian_kernel(X[i], X[j])
+
+            self.G = self.G * np.dot(y, y.T)
+       
         bnds = Bounds(0, self.C)
         cons = {'type': 'eq', 'fun': lambda alphas: np.dot(alphas, y)}
-        alpha_0 = np.random.rand(X.shape[0])
+        alpha_0 = np.zeros(X.shape[0])
 
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore")
@@ -119,10 +134,27 @@ class svm_dual:
 
     def recover_params(self, X, y, alphas):
         self.weights = np.dot(X.T * alphas, y)
-        self.bias = np.mean(y - np.dot(X, self.weights))
+        if self.use_kernel == 0:
+            self.bias = np.mean(y - np.dot(X, self.weights))
+        else:
+            kernel = np.zeros((X.shape[0], X.shape[0]))
+            for i in range(X.shape[0]):
+                for j in range(X.shape[0]):
+                    kernel[i,j] = gaussian_kernel(X[i], X[j], self.gamma)
+            self.bias = np.mean(y - alphas*y*kernel)
         
     def predict(self, X, y):
-        predictions = np.sign(np.dot(X, self.weights) + self.bias)
+        if self.use_kernel == 0:
+            predictions = np.sign(np.dot(X, self.weights) + self.bias)
+        else:
+            predictions = np.zeros((X.shape[0]))
+            for i in range(X.shape[0]):
+                kernel = np.zeros((1, X.shape[0]))
+                for j in range(X.shape[0]):
+                    kernel[0, j] = gaussian_kernel(X[i], X[j], self.gamma)
+                predictions[i] = np.sum(self.alphas[i] * y[i] * np.sum(kernel))
+            predictions = np.sign(predictions + self.bias)
+
         errors = 0
         for i in range(X.shape[0]):
             if predictions[i] != y[i]:
@@ -133,15 +165,18 @@ class svm_dual:
 
 if __name__ == "__main__":
 
+    ###################################
+    # read data and setup hyperparams #
+    ###################################
+    train, test, y, y_test = read_csv()
+    C = [100/873, 500/873, 700/873]
+
     ##############
     # svm primal #
     ##############
     print("SVM Primal Domain:\r\n")
-
-    C = [100/873, 500/873, 700/873]
     print("Learning Rate Schedule 0\r\n")
     for param in C:
-        train, test, y, y_test = read_csv()
         model = svm(C=param, lr_schedule=0)
         model.fit(train, y)
         print(f"For C: {param}")
@@ -152,7 +187,6 @@ if __name__ == "__main__":
 
     print("Learning Rate Schedule 1\r\n")
     for param in C:
-        train, test, y, y_test = read_csv()
         model = svm(C=param, lr_schedule=1)
         model.fit(train, y)
         print(f"For C: {param}")
@@ -164,15 +198,34 @@ if __name__ == "__main__":
     ############
     # svm dual #
     ############
-    print("SVM Dual Domain:")
+    print("SVM Dual Domain: This will take a few minutes to complete\r\n")
 
-    train, test, y, y_test = read_csv()
-    model = svm_dual()
-    model.fit(train, y)
-    model.recover_params(train, y, model.alphas)
-    print(f"Recovered Weights: {model.weights.T}")
-    print(f"Recovered Bias: {model.bias}")
-    print(f"Train Error: {round(model.predict(train, y) * 100, 3)}%")
-    print(f"Test Error: {round(model.predict(test, y_test) * 100, 3)}%")
+    for param in C:
+        model = svm_dual(C=param, use_kernel=0)
+        model.fit(train, y)
+        model.recover_params(train, y, model.alphas)
+        print(f"For C: {param}")
+        print(f"Recovered Weights: {model.weights.T}")
+        print(f"Recovered Bias: {model.bias}")
+        print(f"Train Error: {round(model.predict(train, y) * 100, 3)}%")
+        print(f"Test Error: {round(model.predict(test, y_test) * 100, 3)}%")
+        print("\r\n")
+
+    ###############################
+    # svm dual w/ Gaussian Kernel #
+    ############################### 
+    G = [0.1, 0.5, 1, 5, 100]
+    print("SVM Dual Domain w/ Gaussian Kernel: This will take a few minutes to complete\r\n")
+    for param in C:
+        for gamma in G:
+            model = svm_dual(C=param, use_kernel=1, gamma=gamma)
+            model.fit(train, y)
+            model.recover_params(train, y, model.alphas)
+            print(f"For C: {param} and gamma: {gamma}")
+            print(f"Recovered Weights: {model.weights.T}")
+            print(f"Recovered Bias: {model.bias}")
+            print(f"Train Error: {round(model.predict(train, y) * 100, 3)}%")
+            print(f"Test Error: {round(model.predict(test, y_test) * 100, 3)}%")
+            print("\r\n")
 
 
